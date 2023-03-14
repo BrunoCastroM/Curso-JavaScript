55897de3e675847e00f038c7cb0ae0dbba58e547
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2023-03-14 18:31:30 -0300

    Aula 97
    Importes para deixar o código mais atual funcionando em navegadores mais antigos

---
+++ aula97-core-js-e-regenerator-runtime/src/main.js
@@ -0,0 +1,8 @@
+// 2 importes para fazer o código mais atual funcionar em navegadores mais antigos
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import executa from './modules/promises';
+import './assets/css/style.css';
+
+executa();
---
+++ aula97-core-js-e-regenerator-runtime/src/modules/promises.js
@@ -0,0 +1,15 @@
+import { async } from "regenerator-runtime";
+
+function promise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Sou a promise');
+            resolve();
+        }, 2000)
+    })
+}
+
+export default async function () {
+    await promise();
+    console.log('terminou')
+}
