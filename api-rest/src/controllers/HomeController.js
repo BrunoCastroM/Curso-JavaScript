5eac5f90ec1d649a8d665e1b72e60aafbaea1dcb
BrunoCastroM <brunocastromoura@gmail.com>
2023-04-12 14:00:35 -0300

    Configurando e testando o Sequelize

--- api-rest/src/controllers/HomeController.js
+++ api-rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Amanda',
+      sobrenome: 'Linhares',
+      email: 'amanda@gmail.com',
+      idade: 31,
+      peso: 51,
+      altura: 1.54,
     });
+    res.json(novoAluno);
   }
 }
 
