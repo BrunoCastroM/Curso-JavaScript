a3317beb204f328fd9cc5339d891603d121de2b8
BrunoCastroM <brunocastromoura@gmail.com>
2023-04-19 11:04:36 -0300

    Criando as rotas dos alunos

--- api-rest/src/controllers/HomeController.js
+++ api-rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Amanda',
-      sobrenome: 'Linhares',
-      email: 'amanda@gmail.com',
-      idade: 31,
-      peso: 51,
-      altura: 1.54,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
