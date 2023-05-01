d812007cb09d7203b2684c13c73f4c05a06e3cc3
BrunoCastroM <brunocastromoura@gmail.com>
2023-05-01 11:41:17 -0300

    Criando a funcionalidade de editar

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -9,21 +9,32 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
     if (tarefas.indexOf(novaTarefa) !== -1) return;
 
-    const novasTarefa = [...tarefas];
+    const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefa, novaTarefa],
-    });
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
   };
 
   handleChange = (e) => {
@@ -33,7 +44,11 @@ export default class Main extends Component {
   };
 
   handleEdit = (e, index) => {
-    console.log('edit', index);
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
   };
 
   handleDelete = (e, index) => {
