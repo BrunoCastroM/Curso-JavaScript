594fdfb534a0b40617ddfa18c2db98cc1fa8afce
BrunoCastroM <brunocastromoura@gmail.com>
2023-05-23 12:49:06 -0300

    Aprendendo react router dom v6

---
+++ react-vite/react-vite/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return (
+        <div>
+            <h1>Post</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur tempora explicabo exercitationem aliquid, in velit quod repellendus quo porro ipsam totam voluptatem magni, eligendi soluta, debitis nam vero ex doloribus!</p>
+        </div>
+    )
+}
